4f106cddcf9df3f0a0be63b09f56544f94fd1202
Rajdeep Sondarva <70204475+Rajdeep-1995@users.noreply.github.com>
2021-07-31 05:53:28 -0400

    Keys removed

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCc3JiQejU9XD7g1G9sFsvm_xVaw2PsLTw",
-  authDomain: "ecommerce-3337b.firebaseapp.com",
-  projectId: "ecommerce-3337b",
-  storageBucket: "ecommerce-3337b.appspot.com",
-  messagingSenderId: "103382981245",
-  appId: "1:103382981245:web:5648f2b985d46ed3b2af14",
+  apiKey: "w",
+  authDomain: "ecomm.firebaseapp.com",
+  projectId: "ecommer",
+  storageBucket: "ecommercb.appspot.com",
+  messagingSenderId: "103382987654",
+  appId: "9:1033877981245:web:5648f2b98655gff",
 };
 
 // initialize firebase app
